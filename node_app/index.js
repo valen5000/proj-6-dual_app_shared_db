56f966cd4526adb152d7f4fe22e57b5a3d97d3cd
Arinze Chinelo Jennifer <valentino.arinze@gmail.com>
2025-04-07 20:40:48 +0100

    change

--- node_app/index.js
+++ node_app/index.js
@@ -12,7 +12,7 @@ const pool = new Pool({
 app.get('/', async (req, res) => {
   const result = await pool.query('SELECT name FROM devs');
   const names = result.rows.map(row => `<li>${row.name}</li>`).join('');
-  res.send(`<h1>Node.js app with shared DB is up and running!, Project done by Arinze Chinelo Jennifer and Damilare Shittu from Group 25!</h1><ul>${names}</ul>`);
+  res.send(`<h1>Node.js app with shared DB is up and running!, Project done by Arinze Chinelo Jennifer, Damilare Shittu and natasha chingwengwezi from Group 25!</h1><ul>${names}</ul>`);
 });
 
 app.listen(3000, () => console.log('Node.js app listening on port 3000'));
